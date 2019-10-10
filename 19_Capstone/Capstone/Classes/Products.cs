385a0444e0a009c16b267063737b90937491d5a6
Glenn Weber <glennweber099@gmail.com>
2019-10-10 14:11:59 -0400

    Whatever this is

--- 19_Capstone/Capstone/Classes/Products.cs
+++ 19_Capstone/Capstone/Classes/Products.cs
@@ -13,27 +13,8 @@ namespace Capstone.Classes
         public static string ProductName { get; private set; }
         public static decimal ProductPrice { get; private set; }
         public static string ProductType { get; private set; }
-        public static int InventoryLevels { private get; set; }
 
-        public static Dictionary<string, int> inventoryLevel = new Dictionary<string, int>()
-        {
-            { "A1", 5 },
-            { "A2", 5 },
-            { "A3", 5 },
-            { "A4", 5 },
-            { "B1", 5 },
-            { "B2", 5 },
-            { "B3", 5 },
-            { "B4", 5 },
-            { "C1", 5 },
-            { "C2", 5 },
-            { "C3", 5 },
-            { "C4", 5 },
-            { "D1", 5 },
-            { "D2", 5 },
-            { "D3", 5 },
-            { "D4", 5 },
-        };
+        public static Dictionary<string, int> inventoryLevel = new Dictionary<string, int>(){};
 
 
         public Products(string location, string productName, decimal productPrice, string productType)
@@ -54,14 +35,13 @@ namespace Capstone.Classes
                 //Directory.SetCurrentDirectory(@"../../../..");
                 using (StreamReader sr = new StreamReader("Inventory.txt"))
                 {
+                    string inputLine = sr.ReadLine();
                     while (!sr.EndOfStream)
                     {
-                        string inputLine = sr.ReadLine();
                         string[] fields = inputLine.Split(separator);
-
                         Products product = new Products(fields[0], fields[1], decimal.Parse(fields[2]), fields[3]);
                         Console.WriteLine($"{fields[0]} - {fields[1]} - ${fields[2]:C}");
-                        products.Add(product);
+                        //inventoryLevel.Add(fields[0], 5);
                     }
                 }
             }
