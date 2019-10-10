364c5c1971f49d83929689acd9e80aaa7bb300a9
Andy Miller <andymiller2886@gmail.com>
2019-10-10 15:40:55 -0400

    small changes

---
+++ 19_Capstone/CapstoneTests/MoneyTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class MoneyTests
+    {
+        private 
+
+        [DataTestMethod]
+        [DataRow(3, "Fizz", DisplayName = "Testing #3")]
+
+
+        public void BalanceTest1(int submittedNumber, string expectedResult)
+        {
+            // Act
+            decimal actualResult
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+    
+}
