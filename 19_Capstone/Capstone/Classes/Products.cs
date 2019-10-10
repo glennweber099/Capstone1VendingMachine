d1a428c4486e9aefd2c8c288f7287ea656cc8a0a
Andy Miller <andymiller2886@gmail.com>
2019-10-10 09:46:55 -0400

    work on inventory page

--- 19_Capstone/Capstone/Classes/Products.cs
+++ 19_Capstone/Capstone/Classes/Products.cs
@@ -6,5 +6,16 @@ namespace Capstone.Classes
 {
     public class Products
     {
+        public string Location { get;  }
+        public string ProductName { get; }
+        public double ProductPrice { get;  }
+        public string ProductType { get; }
+
+        public Product(string location, string productName, double productPrice, string productType)
+        {
+            location = Location;
+            productName = ProductName;
+        }
+
     }
 }
