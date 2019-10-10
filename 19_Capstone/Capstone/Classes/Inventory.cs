a66f2cdf3f521eeac864e74109e6862a4480e8cf
Andy Miller <andymiller2886@gmail.com>
2019-10-10 10:45:41 -0400

    Added to Inventory and Products

--- 19_Capstone/Capstone/Classes/Inventory.cs
+++ 19_Capstone/Capstone/Classes/Inventory.cs
@@ -6,25 +6,51 @@ namespace Capstone.Classes
 {
     public class Inventory
     {
-        public static string location { get; }
+        public static string location { get; private set; }
 
-        public bool checkInventory(string location)
+        public static bool checkInventory(string location)
         {
+            if (amountInStock > 0)
+            
             // TODO Checks if product is in-stock
+            return true;
         }
 
-        public bool checkProductCode(string location)
+        public static bool checkProductCode(string location)
         {
             // TODO Checks if product exists
+            return true;
         }
-        public void dispenseProduct(string location)
+        public static string dispenseProduct(string location)
         {
             // TODO  Display message for different product types
+
+            if (productType == "Chip")
+            {
+                Console.Write("Crunch Crunch, Yum!");
+            }
+
+            if (productType == "Candy")
+            {
+                Console.Write("Munch Munch, Yum!");
+            }
+
+            if (productType == "Drink")
+            {
+                Console.Write("Glug Glug, Yum!");
+            }
+
+
+            if (productType == "Gum")
+            {
+                Console.Write("Chew Chew, Yum!");
+            }
+
         }
-        public void updateInventory(string location)
+        public static void updateInventory(string location)
         {
             // TODO Update amount remaining
-
+            inventoryLevel[Location]--;
         }
     }
 }
--- 19_Capstone/Capstone/Classes/Products.cs
+++ 19_Capstone/Capstone/Classes/Products.cs
@@ -1,21 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Capstone.Classes
 {
     public class Products
     {
+        private const char separator = '|';
+
         public string Location { get;  }
         public string ProductName { get; }
         public double ProductPrice { get;  }
         public string ProductType { get; }
+        public int amountInStock
+        {
+            get
+            {
+                return inventoryLevel[Location];
+            }
+        }
 
-        public Product(string location, string productName, double productPrice, string productType)
+
+        public Products(string location, string productName, double productPrice, string productType)
         {
             location = Location;
             productName = ProductName;
+            productPrice = ProductPrice;
+            productType = ProductType;
         }
 
+        Dictionary<string, int> inventoryLevel = new Dictionary<string, int>()
+        {
+            { "A1", 5 },
+            { "A2", 5 },
+            { "A3", 5 },
+            { "A4", 5 },
+            { "B1", 5 },
+            { "B2", 5 },
+            { "B3", 5 },
+            { "B4", 5 },
+            { "C1", 5 },
+            { "C2", 5 },
+            { "C3", 5 },
+            { "C4", 5 },
+            { "D1", 5 },
+            { "D2", 5 },
+            { "D3", 5 },
+            { "D4", 5 },
+        };
+
+        public void LoadInventory()
+        {
+            // TODO: Load products from the file (Inventory.txt), parse and display inventory
+            try
+            {
+                using (StreamReader sr = new StreamReader("Inventory.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string inputLine = sr.ReadLine();
+                        string[] fields = inputLine.Split(separator);
+
+                        Products product = new Products(fields[0], fields[1], double.Parse(fields[2]), fields[3]);
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Inform the user 
+                Console.WriteLine($"There was an ERROR loading the file {Path}.  The error was {ex.Message}");
+            }
+        }
     }
 }
